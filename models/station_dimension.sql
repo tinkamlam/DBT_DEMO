56f3f5b6adb1733ea26a35531ded95e6341c8b8b
Eric Lam <erictlam@icloud.com>
2026-05-03 16:18:29 +0000

    station dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,12 @@
+WITH BIKE as (
+    select
+    distinct
+    start_statio_id as station_id,
+    start_station_name as station_name,
+    start_lat as station_lat,
+    start_lng as station_lng
+    from {{ source('demo', 'bike') }}
+    where ride_id != 'ride_id'
+)
+select 
+* from BIKE
